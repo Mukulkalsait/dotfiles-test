c0e9dacac98b824e36ac3a0f3ee641a54469f02a
Mukulkalsait <mdk.121.72633.sg@gmail.com>
2025-01-28 09:14:51 +0530

    added phpcsfixer file

---
+++ userFolder/.php-cs-fixer.php
@@ -0,0 +1,16 @@
+<?php
+return (new PhpCsFixer\Config())
+    ->setRules([
+        '@PSR12' => true,
+        'array_syntax' => ['syntax' => 'short'],
+        'ordered_imports' => true,
+        'no_unused_imports' => true,
+        'binary_operator_spaces' => true,
+        'tab_width' => 2,            // How many spaces a tab represents
+        'concat_space' => ['spacing' => 'one'],
+        'line_length' => [
+            'line_length' => 200,  // Change this number to your preferred length
+            'break_long_lines' => false,
+            'inline_short_lines' => false,
+        ],
+    ]);
